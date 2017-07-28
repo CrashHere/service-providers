3962b0a7d01dff2636fb6639e5f6ff80d6c0730a
David Christie <davidchristie.nz@gmail.com>
2017-07-29 11:31:15 +1200

    Geocode service providers

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const geocoder = require('geocoder')
+const path = require('path')
+
+const providers = require('./data/service-providers.json')
+
+const promises = providers.map(provider => {
+  return new Promise((resolve, reject) => {
+    geocoder.geocode(provider.address, (error, data) => {
+      if (data) {
+        const result = data.results[0]
+        if (result) {
+          const geometry = result.geometry
+          const location = geometry.location
+          const geocoded = Object.assign(
+            {},
+            provider,
+            {
+              _geoloc: location
+            }
+          )
+          resolve(geocoded)
+        }
+      }
+      resolve(provider)
+    })
+  })
+})
+
+Promise.all(promises)
+  .then(data => {
+    console.log(data)
+    const json = JSON.stringify(data)
+    console.log('Writing geocoded results...')
+    fs.writeFileSync(
+      path.resolve('src/data/service-providers-geocoded.json'),
+      json
+    )
+  })
+  .catch(error => {
+    console.log(error)
+  })
